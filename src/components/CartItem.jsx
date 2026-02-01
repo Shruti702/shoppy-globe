3e1bfc4030bdf442aab8cd9d6262fecf4a6b6416
Shruti702 <karunakaramshruti@gmail.com>
2026-02-01 23:56:03 +0530

    Added quantity adjustment and remove logic to CartItem.

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,26 +1,35 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { removeFromCart, updateQuantity } from '../store/cartSlice';
 
 const CartItem = ({ item }) => {
+  const dispatch = useDispatch();
+
+  const handleQtyChange = (qty) => {
+    if (qty < 1) return; //Prevent going below 1.
+    dispatch(updateQuantity({ id: item.id, quantity: qty }));
+  };
+
   return (
     <div className="cart-item">
-      {/*Product Thumbnail*/}
+        {/*Product Thumbnail*/}
       <img src={item.thumbnail} alt={item.title} />
-      
       {/*Title and Price*/}
       <div className="item-details">
         <h4>{item.title}</h4>
         <p>${item.price}</p>
       </div>
-      
       {/*Quantity Controls*/}
       <div className="quantity-controls">
-        <button>-</button>
+        <button onClick={() => handleQtyChange(item.quantity - 1)}>-</button>
         <span>{item.quantity}</span>
-        <button>+</button>
+        <button onClick={() => handleQtyChange(item.quantity + 1)}>+</button>
       </div>
-    
-      {/*Remove Button*/}
-      <button className="remove-btn">
+       {/*Remove Button*/}
+      <button 
+        className="remove-btn"
+        onClick={() => dispatch(removeFromCart(item.id))} 
+      >
         Remove
       </button>
     </div>
