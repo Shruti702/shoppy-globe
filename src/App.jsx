82157094e2d93001e38b20a4bdc7e4a7b3f38a81
Shruti702 <karunakaramshruti@gmail.com>
2026-02-02 09:12:55 +0530

    Defined main App layout with Outlet and added a footer.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import './App.css'
+import React from 'react';
+import Header from './components/Header';
+import { Outlet } from 'react-router-dom';
 
 function App() {
-
   return (
     <>
+      <Header />
+      <main className="main-content">
+        <Outlet />
+      </main>
+      <footer className="footer">© 2026 ShoppyGlobe</footer>
     </>
-  )
+  );
 }
 
 export default App;
